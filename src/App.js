0bbdc82f81d5971cac208d0589901d96d9d4636f
Ayush Sinha <ayush.sinha@clevertap.com>
2023-02-01 22:41:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import mParticle from '@mparticle/web-sdk';
+import clevertap from './clevertap-wrapper'
+import { useEffect } from 'react';
+
+// mParticle.init('us1-d6f4fe26d664924ab97c87f951cc5ea8', {
+//   isDevelopmentMode: false,
+//   identifyRequest: {
+//     userIdentities: {
+//       email: 'test1abc@example.com',
+//       customerid: '7890abc',
+//     },
+//   },
+//   forwardWebRequestsServerSide: false
+//   }
+// )
+
+
+const mparticleConfig = mParticle.MPConfiguration = {
+  isDevelopmentMode: false,
+  loglevel: 'verbose',
+  identifyRequest: {
+    userIdentities: {
+      email: 'test1abc@example.com',
+      customerid: '7890abc',
+    },
+  },
+  forwardWebRequestsServerSide: false,
+  dataPlan: {
+    planId: 'my_plan_id',
+    planVersion: 2,
+  },
+  identityCallback: (result) => {
+    if (result.getUser()) {
+      // User has been identified
+      // proceed with any custom logic that requires a valid, identified user
+
+      const user = result.getUser();
+      const { userIdentities } = user.getUserIdentities();
+      console.log({user})
+      // For demonstration purposes, we are printing out the known values for a user
+      // to the console. An example of a use case for this callback might be to sync
+      // the identity of a user with your own authentication logic
+      Object.keys(userIdentities).forEach((identity) => {
+        console.log(
+          'User Identity Value: ',
+          identity,
+          userIdentities[identity]
+        );
+      });
+    } else {
+      // the IDSync call failed
+    }
+  },
+}
 
 function App() {
+  useEffect(() => {
+    mParticle.init('us1-d6f4fe26d664924ab97c87f951cc5ea8', mparticleConfig)
+  }, [])
+
+  mParticle.logEvent('React Web Test', mParticle.EventType.Navigation, {
+    movie_length: '127 minutes',
+    rating: 'PG',
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +72,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
