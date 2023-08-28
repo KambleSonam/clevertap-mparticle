398085980ecc5f46d1d5308db7f6410a3ee410ea
KambleSonam <sonam@clevertap.com>
2023-08-28 17:54:18 +0530

    code cleanup

--- src/App.js
+++ src/App.js
@@ -61,18 +61,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Clevertap mParticle Sample App
         </p>
-        {/* <button onClick={initMpart}>Init SDK</button> */}
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
